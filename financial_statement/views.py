ce9cdd6d2fb485fbd6e6f7acd309df97047d8dd3
xjjwang <xjjwang@outlook.com>
2024-06-08 19:28:16 +0800

    项目现在可以渲染到html

--- financial_statement/views.py
+++ financial_statement/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import BasicInfo
 
 def index(request):
     first_object = BasicInfo.objects.first()
     name = first_object.name
-    return HttpResponse("The project name is %s" % name)
+    return render(request, 'financial_statement/index.html', {'name': name})
     # 导入模型
